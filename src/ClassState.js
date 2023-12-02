8321feb1e8c12ecafce4fbc70d6f7b1c1504f38a
Jose Préndiz <jprendiz@gmail.com>
2023-12-01 18:38:43 -0600

    Estados compuestos imperativos en ClassState

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,31 +1,39 @@
 import React from "react"
 import { Loading } from './Loading'
 
+const SECURITY_CODE = 'paradigma'
+
 class ClassState extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      error: true,
-      loading: true,
+      value: '',
+      error: false,
+      loading: false,
     }
   }
 
   // UNSAFE_componentWillMount() {
-  //   console.log("componentWillMount");
-  // };
-  // componentDidMount() {
-  //   console.log("componentDidMount");
-  // };
-  
-  componentDidUpdate() {
-    console.log("Actualización...");
+    //   console.log("componentWillMount");
+    // };
+    // componentDidMount() {
+      //   console.log("componentDidMount");
+      // };
+      
+      componentDidUpdate() {
+        console.log("Actualización...");
+        console.log(this.state.value);
 
     if ( this.state.loading ) {
       setTimeout(() => {
         console.log("Haciendo la validación");
       
-        this.setState({ loading: false })
+        if ( SECURITY_CODE === this.state.value) {
+          this.setState({ loading: false, error: false })
+        } else {
+          this.setState({error: true, loading: false})
+        }
       
         console.log("terminando la validación");
       }, 3000);
@@ -33,22 +41,30 @@ class ClassState extends React.Component {
   }
 
   render () {
+    const { error, loading, value } = this.state;  // haciendo destructuración para simplificar el código
     return (
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>Por favor escriba el código de seguridad: </p>
 
-        { this.state.error && 
+        { error && !loading &&
           <p>El código es incorrecto</p> 
         }
 
-        { this.state.loading && 
+        { loading && 
           <Loading /> 
         }
 
-        <input placeholder="Código de seguridad" />
+        <input 
+          placeholder="Código de seguridad" 
+          value={value}
+          onChange={(e) => { 
+            this.setState( {value: e.target.value} )
+          }}
+          
+        />
         <button
-          onClick={() => (this.setState( prevState => ({loading: false }) ) ) }
+          onClick={() => (this.setState( { loading: true }) ) }
         >Comprobar
         </button>
       </div>
